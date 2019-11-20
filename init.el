61a9829722f1c80df97db042b51f3da2c943be1d
Hunter Jones <hjones2199@gmail.com>
2019-11-20 02:28:06 -0600

    Added byte compiled version of the config file generated by org

--- init.el
+++ init.el
@@ -8,7 +8,13 @@
 ;; Adds invicta config to the loadpath
 (push (expand-file-name (locate-user-emacs-file "invicta")) load-path)
 (require 'invictainit)
-(org-babel-load-file (locate-user-emacs-file "psimacs.org"))
+
+;; Loads the psimacs configuration, tangling and compiling the
+;; file if it does not exist.
+(let ((psimacs-file (expand-file-name (concat user-emacs-directory "psimacs.elc"))))
+  (if (file-exists-p psimacs-file)
+      (load psimacs-file)
+    (org-babel-load-file (expand-file-name (concat user-emacs-directory "psimacs.org")) t)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
