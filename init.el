3fc2eb02a68b7a620c715a366f63728d8c4b11e6
hjones2199 <hjones2199@gmail.com>
2019-11-23 11:55:24 -0600

    Added icon support for Ivy & dired, and lsp-ui

--- init.el
+++ init.el
@@ -25,12 +25,3 @@
  '(menu ((t (:foreground "brightblack" :inverse-video t))))
  '(swiper-line-face ((t (:background "dim gray" :foreground "#1c1f24"))))
  '(tty-menu-selected-face ((t (:background "color-208")))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (lsp-ui use-package treemacs-projectile treemacs-magit slime-company pyvenv org-plus-contrib org-bullets lsp-origami lsp-java god-mode go-snippets go-mode eshell-toggle doom-themes doom-modeline dashboard dap-mode counsel company-lsp company-box centaur-tabs bar-cursor auto-package-update anaconda-mode))))
